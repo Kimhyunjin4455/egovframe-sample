426f4408e7d6ad9852f2eda56ea7acc9fd810658
wnsxk2 <wnsxk206@gmail.com>
2025-04-30 06:39:24 +0900

    project init

---
+++ src/main/java/com/example/egovframesample/EgovframeSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.egovframesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgovframeSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgovframeSampleApplication.class, args);
+    }
+
+}
