d70000a03e02eca300222db82134c430636089b0
Kimhyunjin4455 <dp4455@naver.com>
2025-05-01 22:18:46 +0900

    build: eGovFramework 셋팅 + log4j 셋팅

--- src/main/java/com/example/egovframesample/EgovframeSampleApplication.java
+++ src/main/java/com/example/egovframesample/EgovframeSampleApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EgovframeSampleApplication {
+public class EgovframeSampleApplication  {
+    // extends SpringBootServletInitializer 는 war일 때
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(EgovframeSampleApplication.class);
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(EgovframeSampleApplication.class, args);
